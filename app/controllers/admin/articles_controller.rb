0e9150745fb6f3a3b1135cc204929f8ad44ef770
Dave Rapin <dave@rapin.com>
2012-06-03 17:18:33 -0400

    fixed up the article views

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -3,34 +3,34 @@ class Admin::ArticlesController < Admin::HomeController
 
   def index
     @articles = Article.all
-    respond_with(@articles)
+    respond_with(:admin, @articles)
   end
 
   def show
     @article = Article.find(params[:id])
-    respond_with(@article)
+    respond_with(:admin, @article)
   end
 
   def new
     @article = Article.new
-    respond_with(@article)
+    respond_with(:admin, @article)
   end
 
   def edit
     @article = Article.find(params[:id])
-    respond_with(@article)
+    respond_with(:admin, @article)
   end
 
   def create
     @article = Article.new(params[:article])
     @article.save
-    respond_with(@article)
+    respond_with(:admin, @article)
   end
 
   def update
     @article = Article.find(params[:id])
     @article.update_attributes(params[:article])
-    respond_with(@article)
+    respond_with(:admin, @article)
   end
 
   def destroy
@@ -38,7 +38,7 @@ class Admin::ArticlesController < Admin::HomeController
     @article.destroy
 
     respond_to do |format|
-      format.html { redirect_to articles_url }
+      format.html { redirect_to admin_articles_url }
       format.json { head :no_content }
     end
   end
